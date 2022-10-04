d430fc336d1f4bd4a762e1ef1fafc306bb8fa3c8
Mariia <martush1992@gmail.com>
2022-10-04 13:23:00 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Введите число M:");
+int numM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N:");
+int numN = int.Parse(Console.ReadLine());
+
+
+void intervalNumberSum (int numM, int numN, int sum)
+{
+    if (numM > numN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в интервале от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numM++);
+    intervalNumberSum(numM, numN, sum);
+}
+
+intervalNumberSum(numM, numN, 0);
